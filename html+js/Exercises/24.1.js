f6d77a6b1a5581dd7ec63b624bc691f44b30c9dd
BlackZero172 <Supermeega@outlook.com>
2021-12-06 12:11:04 +0200

    Section 24

---
+++ html+js/Exercises/24.1.js
@@ -0,0 +1,12 @@
+function Square(a, b, c, d) {
+	this.a = a;
+	this.b = b;
+	this.c = c;
+	this.d = d;
+	this.isSquare = () => {
+		return a === b && b === c && c === d;
+	};
+}
+const square = new Square(2, 2, 1, 1);
+
+console.log(square.isSquare());
